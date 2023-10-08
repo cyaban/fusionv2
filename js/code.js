eb9e6040ef4f6c0f4290e4afb9a1dabb636fdd0b
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-10-08 12:27:22 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,57 +1,11 @@
-function makeDefaultBlockCode(json_data) {
-  // Generates a default block code string from the given JSON data.
+// Load the JSON object
+const jsonData = fetch('games.json').then(response => response.json());
 
-  // Args:
-  //   json_data: A JSON object.
+// Generate the HTML code for the document
+jsonData.then(data => {
+  const template = Handlebars.compile(document.getElementById('template').innerHTML);
+  const html = template({ data });
 
-  // Returns:
-  //   A JSON object.
-
-  return json_data;
-}
-
-function loopJsonContentsAndMakeStringWithDefaultBlockCode(json_contents) {
-  // Loops through the given JSON contents and makes a string with the default
-  // block code for each item, replacing the image URL, URL, and onclick to what
-  // the JSON is.
-
-  // Args:
-  //   json_contents: A list of JSON objects.
-
-  // Returns:
-  //   A list of JSON objects.
-
-  return json_contents;
-}
-
-// Example usage:
-
-const json_contents = [
-  {
-    "swiper-slide": {
-      "box": {
-        "box-text": {
-          "img": {
-            "src": "images/1C15ABFC-CB62-4115-B050-D693649AE1EC.jpeg",
-            "alt": "Pacman"
-          },
-          "h2": "Pacman",
-          "h3": "Action",
-          "a": {
-            "href": "./iframe.html",
-            "onclick": "localStorage.setItem('iframeurl', 'embed.html#https://cyamanz.github.io/play/flash/?swf=pacman.swf'); localStorage.setItem('staturl', './iframe.html')",
-            "class": "box-btn",
-            "i": {
-              "class": "fa-solid fa-download"
-            }
-          }
-        }
-      }
-    }
-  }
-];
-
-const default_block_code_json = loopJsonContentsAndMakeStringWithDefaultBlockCode(json_contents);
-
-console.log(default_block_code_json);
-
+  // Add the HTML code to the document body
+  document.querySelector('.swiper-wrapper').innerHTML = html;
+});
