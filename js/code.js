3ad1ef34fba9d130ed4e6926bd70b261ae76106b
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-10-08 09:06:08 -0500

    Create code.js

---
+++ js/code.js
@@ -0,0 +1,11 @@
+// Load the JSON object
+const jsonData = fetch('games.json').then(response => response.json());
+
+// Generate the HTML code for the document
+jsonData.then(data => {
+  const template = Handlebars.compile(document.getElementById('template').innerHTML);
+  const html = template({ data });
+
+  // Add the HTML code to the document body
+  document.querySelector('.swiper-wrapper').innerHTML = html;
+});
