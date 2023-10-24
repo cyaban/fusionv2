6e848aebe18098abb92742b92bb814919ecc416d
Nate <nathaniel_ramirez@icloud.com>
2023-10-23 19:15:56 -0700

    add href to game card and fix navigation

--- js/game_cards.js
+++ js/game_cards.js
@@ -8,10 +8,14 @@ fetch("/js/json/games.json").then(function (response) {
       const gametext = document.createElement("h3");
       const gameimg = document.createElement("img");
       const gamedesc = document.createElement("p");
+      const wrapper = document.createElement("a");
       gametext.innerText = game.name;
       gamedesc.innerText = game.description;
+      wrapper.href = game.href;
       gameimg.src = game.img;
-      cards.appendChild(card);
+      cards.appendChild(wrapper);
+      wrapper.appendChild(card);
+      wrapper.style.textDecoration = "none"; // remove the stupid underline since its a link
       card.appendChild(innerdiv);
       card.prepend(gameimg); // puts at top of inside div, append puts at bottom
       innerdiv.appendChild(gametext);
