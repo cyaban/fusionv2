bfd1fa6ba919b3e81ba1455aa87c46fd97d5811c
Cyabanz <79590387+cyaban@users.noreply.github.com>
2023-11-08 08:32:35 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://soonwithfreedns.onrender.com/outerspace/',
+    bare: 'https://nyxosistheworst.onrender.com/outerspace/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
